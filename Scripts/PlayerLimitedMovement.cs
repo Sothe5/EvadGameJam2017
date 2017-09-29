8a8da401b2600d39e8680535774ba32a5c06ce29
Alvaro <almaral4c@gmail.com>
2017-09-30 00:32:06 +0200

    Props added and PlayerLimited script

---
+++ Scripts/PlayerLimitedMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLimitedMovement: MonoBehaviour {
+
+	private float StartingPoint;
+	// Use this for initialization
+	void Start () {
+		 StartingPoint = FindObjectOfType<ProceduralGeneration> ().transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(this.transform.position.x,this.transform.position.y,StartingPoint);
+	}
+}
