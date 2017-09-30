36c09e92c2d95f91636329cb90b57241befa6243
Nexxel <sergiogonzalezmuriel@gmail.com>
2017-09-30 11:28:27 +0200

    Pause and restart in dead

---
+++ Scripts/RestartInDead.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartInDead : MonoBehaviour {
+
+    public GameObject deadPanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (deadPanel.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Time.timeScale = 1;
+                SceneManager.LoadScene(gameObject.scene.name);
+            }
+        }
+    }
+}
