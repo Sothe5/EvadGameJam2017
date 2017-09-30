1982e361b294cfa00fc958302148572a65303f88
David Ponce <zolastrodavid@gmail.com>
2017-09-30 11:18:51 +0200

    Add meta data

--- Scripts/Chunk.cs
+++ Scripts/Chunk.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour {
 	public float length;
+
+	void Start() {
+		BoxCollider collider = GetComponent<BoxCollider> ();
+		if (collider) {
+			this.length = collider.size.z * this.transform.localScale.z;
+		}
+	}
 }
