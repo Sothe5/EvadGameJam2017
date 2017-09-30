37a1f9943534f062575646ce11befabd39628475
Nexxel <sergiogonzalezmuriel@gmail.com>
2017-09-30 12:19:15 +0200

    Better RestartinDead

--- Scripts/RestartInDead.cs
+++ Scripts/RestartInDead.cs
@@ -6,16 +6,21 @@ using UnityEngine.SceneManagement;
 public class RestartInDead : MonoBehaviour {
 
     public GameObject deadPanel;
+    public Camera footCamera;
+
+    private bool loading;
 
     // Update is called once per frame
     void Update()
     {
-        if (deadPanel.activeInHierarchy)
+        if (deadPanel.activeInHierarchy && !loading)
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
+                loading = true;
+                footCamera.enabled = false;
                 Time.timeScale = 1;
-                SceneManager.LoadScene(gameObject.scene.name);
+                SceneManager.LoadScene(0);
             }
         }
     }
