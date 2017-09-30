6e5ead3d8cb2438eece2fad6be362a8364f218e4
Nexxel <sergiogonzalezmuriel@gmail.com>
2017-09-30 21:19:13 +0200

    It's raining day (^.^)

---
+++ Scripts/Raining.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raining : MonoBehaviour {
+
+    public Animator animator;
+    public float timeBetweenFases;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        animator.SetInteger("inicio", Random.Range(1,4));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= timeBetweenFases)
+        {
+            int random = Random.Range(1, 4);
+            animator.SetInteger("inicio", random);
+            Debug.Log("Random: " + random);
+            timer = 0;
+        }
+	}
+}
